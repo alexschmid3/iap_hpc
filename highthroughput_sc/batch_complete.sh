8525d1a3bee9b4886057fb265293133e9be705b2
Alex Schmid <alexandriaschmid3@gmail.com>
2023-02-01 15:36:34 -0500

    Fix SC batch job

---
+++ highthroughput_sc/batch_complete.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH -a 1-50
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=2G
+#SBATCH --partition=xeon-p8
+#SBATCH --time=0-00:10
+#SBATCH -o /home/aschmid/mydir/outerr/run_\%a.out
+#SBATCH -e /home/aschmid/mydir/outerr/run_\%a.err
+
+#Load software
+module load julia/1.7.3
+
+#Run the script as usual
+julia shortestpath_many.jl $SLURM_ARRAY_TASK_ID
