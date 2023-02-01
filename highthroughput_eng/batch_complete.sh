ba24f653dfb9ef712c9e6fc06aa244c92968ee33
Alex Schmid <alexandriaschmid3@gmail.com>
2023-02-01 16:32:55 -0500

    Fix Engaging file path

--- highthroughput_eng/batch_complete.sh
+++ highthroughput_eng/batch_complete.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-#SBATCH -a 1-10
+#SBATCH -a 1-5
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=2G
 #SBATCH --partition=sched_any_quicktest
 #SBATCH --time=0-00:10
-#SBATCH -o /home/aschmid/mydir/outerr/run_\%a.out
-#SBATCH -e /home/aschmid/mydir/outerr/run_\%a.err
+#SBATCH -o /home/aschmid/iap_hpc/highthroughput_eng/run_\%a.out
+#SBATCH -e /home/aschmid/iap_hpc/highthroughput_eng/run_\%a.err
 
 #Load software
 module load julia/1.7.3
-module load gurobi/9.0.3
 
 #Run the script as usual
 julia shortestpath_many.jl $SLURM_ARRAY_TASK_ID
